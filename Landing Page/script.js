de50909d027f3c9e39872dda4487b2f6dcf4388a
aikadxuk <129562184+aikadxuk@users.noreply.github.com>
2023-09-16 22:46:13 -0300

    Delete Landing Page directory

--- Landing Page/script.js
+++
@@ -1,8 +0,0 @@
-const hamburguer = document.querySelector('.hamburguer')
-const navAll = document.querySelector('.nav-all')
-
-hamburguer.addEventListener('click', open)
-
-function open(){
-    navAll.classList.toggle('open')
-}
